b993e83b56b20b5631634c19b037c06193281f52
Takuya-ops <hora2112.ipu@gmail.com>
2023-12-15 23:08:53 +0900

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}
